c5ed22d048567b4479c9ff45fbb59aa0c3f067d5
Nicholas Munson <nmunson@gmail.com>
2012-03-27 23:14:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Poffpool::Application.config.secret_token = 'b31cd9c49c51654f16320d7c6946e6a0611b490a5e954e405feb13749b04044baa0bb29048f4da2ba1710e4f20a80badd35944265dc0711a9173193fdde0d5b7'
