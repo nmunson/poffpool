f44fbf35895a30b52a533168f39c22f07c670b92
Nicholas Munson <nmunson@gmail.com>
2012-03-28 21:51:18 -0400

    Start model tests

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -3,4 +3,6 @@ class Entrant < ActiveRecord::Base
   has_many :picks
   has_many :players, :through => :picks
 
+  validates :name, :presence => true
+
 end
--- spec/models/entrant_spec.rb
+++ spec/models/entrant_spec.rb
@@ -1,5 +1,36 @@
 require 'spec_helper'
 
 describe Entrant do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before(:each) do 
+    @attr = {
+      :name => "John Smith",
+      :email => "user@example.com",
+      :paid => false
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+    Entrant.create!(@attr)
+  end
+
+  it "should require a name" do
+    no_name_entrant = Entrant.new(@attr.merge(:name => ""))
+    no_name_entrant.should_not be_valid
+  end
+
+  it "should reject duplicate names"
+
+  it "should require an email address"
+
+  it "should reject a name that is too long"
+
+  it "should accept valid email addresses"
+
+  it "should reject invalid email addresses"
+
+  it "should respond to players"
+
+  it "should respond to picks"
+
 end
