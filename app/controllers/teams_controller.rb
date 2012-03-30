8e5a436f6c1398b186b7db91ed646c317d440719
Nicholas Munson <nmunson@gmail.com>
2012-03-30 17:34:12 -0400

    Don't return created_at and updated_at fields

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,12 +4,12 @@ class TeamsController < ApplicationController
 
   def index
     @teams = Team.all
-    respond_with(@teams)
+    respond_with(@teams, :except => [:created_at, :updated_at])
   end
 
   def show
     @team = Team.find(params[:id])
-    respond_with(@team)
+    respond_with(@team, :except => [:created_at, :updated_at])
   end
 
 end
