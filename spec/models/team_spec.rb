84e5437eecf78618ec45b8da1ea2b828b75f756a
Nicholas Munson <nmunson@gmail.com>
2014-04-06 14:21:39 -0400

    Add attributes context

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -9,24 +9,25 @@ describe Team do
     }
   end
 
-  it "should create a new instance given valid attributes" do
-    Team.create!(@attr)
-  end
+  context "attributes" do
+    it "should create a new instance given valid attributes" do
+      Team.create!(@attr)
+    end
 
-  it "should require a name" do
-    no_team_name = Team.new(@attr.merge({:name => ""}))
-    no_team_name.should_not be_valid
-  end
+    it "should require a name" do
+      no_team_name = Team.new(@attr.merge({:name => ""}))
+      no_team_name.should_not be_valid
+    end
 
-  it "should reject duplicate names" do 
-    Team.create!(@attr)
-    duplicate_team = Team.new(@attr)
-    duplicate_team.should_not be_valid
-  end
+    it "should reject duplicate names" do 
+      Team.create!(@attr)
+      duplicate_team = Team.new(@attr)
+      duplicate_team.should_not be_valid
+    end
 
-  it "should require a shortname" do
-    no_shortname = Team.new(@attr.merge({:shortname => ""}))
-    no_shortname.should_not be_valid
+    it "should require a shortname" do
+      no_shortname = Team.new(@attr.merge({:shortname => ""}))
+      no_shortname.should_not be_valid
+    end
   end
-
 end
