2cb6d44a9f8818f49e747007d8c61d9612a3857d
Nicholas Munson <nmunson@gmail.com>
2014-04-13 12:11:43 -0400

    Update wording, show updated list of injuries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,7 @@ class PagesController < ApplicationController
   skip_before_filter :entry_mode_check, :only => [:index, :guide]
 
   def index
+    @injuries = ENV['INJURIES'].split(",")
   end
 
   def prizes
