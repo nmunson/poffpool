32dc32dd365341f7d7caa8d17c317f58bac7a27a
Nicholas Munson <nmunson@gmail.com>
2014-04-18 16:54:55 -0400

    Simplify homepage messaging

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,19 @@ class PagesController < ApplicationController
 
   def index
     @injuries = ENV['INJURIES'].split(",")
+
+    # choose what status the pool is in
+    @pool_status = if Time.parse(ENV['SUBMISSION_DEADLINE']) > Time.now
+      'accepting_entries'
+    elsif Time.parse(ENV['SUBMISSION_DEADLINE']) + 2.days > Time.now
+      'validating_entries'
+    elsif ENV['GAMES_FINISHED'] == 'true'
+      'validating_winners'
+    elsif ENV['WINNERS_DECIDED'] == 'true'
+      'pool_finished'
+    else
+      'pool_active'
+    end
   end
 
   def prizes
