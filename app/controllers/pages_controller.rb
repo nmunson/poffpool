5dd6f5af34eef0afab9979f412ec9fa11042b391
Nicholas Munson <nmunson@gmail.com>
2014-04-25 21:50:34 -0400

    Content for home page now based on environment vars

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,37 @@ class PagesController < ApplicationController
     @prize_money = prize_money
     @updates = updates(@pool_status)
     @injuries = injuries(@pool_status)
+    @welcome_title = ''
+    if @pool_status == 'accepting_entries'
+      @welcome_title = "The playoffs are back!"
+      @welcome_message = "Welcome to the #{Time.now.year} NHL Playoff Pool. All entries "\
+      "must be submitted online before "\
+      "#{Time.parse(ENV['SUBMISSION_DEADLINE']).in_time_zone('Eastern Time (US & Canada)').strftime("%B %-d, %Y at %l:%M %p")} "\
+      "through this website.  The submission of entries past that time are blocked from processing, "\
+      "so please do not leave it until the last minute to submit.  When completing the entry you must "\
+      "ensure that you have made arrangements for your entry fee to be paid.  If not, your name and "\
+      "entry will be removed from the pool."
+      @welcome_link = view_context.link_to "Submit Entry &raquo;".html_safe, new_entrant_path, :class => 'btn btn-primary btn-large'
+    elsif @pool_status == 'validating_entries'
+      @welcome_title = "Entry period has closed."
+      @welcome_message = "We will now be validating all entries.  Feel free to browse "\
+        "the site and see other people's picks.  Once NHL.com begins updating playoff statistics, "\
+        "you'll see the #{view_context.link_to 'rankings page', entrants_path} updated."
+      @welcome_link = view_context.link_to "View Entries &raquo;".html_safe, entrants_path, :class => 'btn btn-primary btn-large'
+    elsif @pool_status == 'pool_active'
+      @welcome_title = "Good luck!"
+      @welcome_message = "Thanks for entering the playoff pool for 2014 and good luck to everyone."
+      @welcome_link = view_context.link_to "View Rankings &raquo;".html_safe, entrants_path, :class => 'btn btn-primary btn-large'
+    elsif @pool_status == 'validating_winners'
+      @welcome_title = "The playoffs are over!"
+      @welcome_message = "All the games have now finished.  We will be double checking the statistics "\
+      "from NHL.com before officially announcing the winners."
+    elsif @pool_status == 'pool_finished'
+      @welcome_title = "Congrats to the winners!"
+      @welcome_message = "The pool is now over and congratulations to all the winners.  Thanks everyone "\
+      "for playing!"
+    end 
+
   end
 
   def prizes
@@ -35,10 +66,10 @@ class PagesController < ApplicationController
       'accepting_entries'
     elsif Time.parse(ENV['SUBMISSION_DEADLINE']) + 2.days > Time.now
       'validating_entries'
-    elsif ENV['GAMES_FINISHED'] == 'true'
-      'validating_winners'
     elsif ENV['WINNERS_DECIDED'] == 'true'
       'pool_finished'
+    elsif ENV['GAMES_FINISHED'] == 'true'
+      'validating_winners'
     else
       'pool_active'
     end
